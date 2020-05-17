7cc1c1c17488036ec6b343237ac7268aa4515f7a
Lachezar Kutsarov <lkutsarov@gmail.com>
2020-05-17 20:11:04 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,60 @@
+package main.caesarCipher;
+
+import java.util.Scanner;
+
+public class Challenge {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String originalMessage = scanner.nextLine();
+    int total_shifts = scanner.nextInt();
+    scanner.close();
+
+    String result = caesarCipher_fromPlainText_toCipherText(originalMessage, total_shifts);
+    System.out.println(result);
+  }
+
+  /**
+   * Encodes the original message by the method of Caesar's Cipher: each character in the original
+   * message is shifted to the right of the alphabet. If the total number of shifts exceeds the
+   * number of letters in the alphabet, then it continues counting from the first alphabet character
+   * again, and so on, until the total number of shifts is reached.
+   *
+   * <p>The positions of the punctuation marks, spaces and the letter case is as in the original
+   * message.
+   *
+   * @return A string, representing the ecnoded message.
+   */
+  private static String caesarCipher_fromPlainText_toCipherText(
+      String plain_text, int total_shifts) {
+
+    StringBuilder cipher_text = new StringBuilder();
+
+    for (int i = 0; i < plain_text.length(); i++) {
+
+      char ch = plain_text.charAt(i);
+      if (Character.isLetter(ch)) {
+        cipher_text.append(findCipherChar(ch, total_shifts));
+      } else {
+        cipher_text.append(ch);
+      }
+    }
+    return cipher_text.toString();
+  }
+
+  private static char findCipherChar(char char_text, int total_shifts) {
+
+    // The method is called only if the char is already confirmed as a letter.
+    // Therefore, if the code point is greater than 96, then it is lowercase.
+    // Otherwise, it is uppercase.
+    int codePoint_for_a_or_for_A = (int) char_text > 96 ? (int) 'a' : (int) 'A';
+    int alphabetPosition_char_text = (int) char_text - codePoint_for_a_or_for_A;
+    int alphabetPosition_char_cipher = alphabetPosition_char_text + total_shifts;
+
+    if (alphabetPosition_char_cipher < 26) {
+      return (char) (codePoint_for_a_or_for_A + alphabetPosition_char_cipher);
+    }
+
+    return (char) (codePoint_for_a_or_for_A + (alphabetPosition_char_cipher % 26));
+  }
+}
