ae12733330eefeaae42e130f70f04cdaba5c711a
Lachezar Kutsarov <lkutsarov@gmail.com>
2020-05-17 20:16:51 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,10 +1,9 @@
-package main.caesarCipher;
-
 import java.util.Scanner;
 
-public class Challenge {
+public class Solution {
 
   public static void main(String[] args) {
+    
     Scanner scanner = new Scanner(System.in);
     String originalMessage = scanner.nextLine();
     int total_shifts = scanner.nextInt();
@@ -16,17 +15,16 @@ public class Challenge {
 
   /**
    * Encodes the original message by the method of Caesar's Cipher: each character in the original
-   * message is shifted to the right of the alphabet. If the total number of shifts exceeds the
-   * number of letters in the alphabet, then it continues counting from the first alphabet character
+   * message is shifted to the right of the alphabet. If the total number of shifts exceeds 
+   * the number of letters in the alphabet, then it continues counting from the first alphabet character
    * again, and so on, until the total number of shifts is reached.
    *
-   * <p>The positions of the punctuation marks, spaces and the letter case is as in the original
-   * message.
+   * The position of the punctuation marks, spaces and the letter case in the encrypted text
+   * is preserved as in the original message.
    *
-   * @return A string, representing the ecnoded message.
+   * @return A string, representing the encrypted message.
    */
-  private static String caesarCipher_fromPlainText_toCipherText(
-      String plain_text, int total_shifts) {
+  private static String caesarCipher_fromPlainText_toCipherText(String plain_text, int total_shifts) {
 
     StringBuilder cipher_text = new StringBuilder();
 
@@ -44,9 +42,9 @@ public class Challenge {
 
   private static char findCipherChar(char char_text, int total_shifts) {
 
-    // The method is called only if the char is already confirmed as a letter.
-    // Therefore, if the code point is greater than 96, then it is lowercase.
-    // Otherwise, it is uppercase.
+    // The method is called only if the character is already confirmed as a letter.
+    // Therefore, if the code point is greater than 96, then it is a lowercase character. 
+    // Otherwise, it is an uppercase character.
     int codePoint_for_a_or_for_A = (int) char_text > 96 ? (int) 'a' : (int) 'A';
     int alphabetPosition_char_text = (int) char_text - codePoint_for_a_or_for_A;
     int alphabetPosition_char_cipher = alphabetPosition_char_text + total_shifts;
